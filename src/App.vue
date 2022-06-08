cff788833c717b8b01239c61543c6c82d4454b14
Tugran Kenger <turankenger@hotmail.com.tr>
2022-06-08 23:48:46 +0300

    vue3 ref(reactive effect) counter, input event, etc. exercises

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,66 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <!-- optiopns api ile composition api setup kullanilarak birlikte kullanilabilir. -->
+
+    {{ title }}
+
+    <input type="text" v-model="title"> <br><br>
+    <button @click="toggleIt">Show/hide</button><br><br>
+    <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore consectetur id odio harum voluptatum dolores optio ratione officiis obcaecati aut nam officia nesciunt cumque assumenda eius similique perspiciatis, eligendi quasi.</p>
+
+    <button @click="counter ++">+</button> &nbsp;
+    <span>{{ counter }}</span> &nbsp;
+    <button @click="counter --">-</button> <br><br>
+    <span style="color:red" v-if="counter < 0">{{smallerThanZero}}</span>
+    <span style="color:green" v-if="counter > 0">{{biggerThanZero}}</span>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+// reactive bir event icin {ref} import edilmeli(reactivite effect (ref) )
+import {ref} from "vue";
+  export default{
+    // data(){
+    //   return{
+    //     title: "this is a title",
+    //   };
+    // },
+    // beforeCreate(){
+    //   console.log("beforeCreate");
+    // },
+    // created(){
+    //   console.log("created");
+    // },
+
+    setup(){ // setup fonksiyonu data, beforecreate, created metodlarinin yerine geldi.
+              // yukaridaki beforecreate ve created metodlarindan once setup calisiyor.
+              //  template asamasinda yani mount oldugunda beforecreate ve created calisir.
+              // setup amaci data'daki reactivity henuz olusmadan once calismak ve data'yi ona gore olusturmak
+              // setup fonksiyonu geriye bir obje return etmeli
+
+      //console.log("setup")
+
+      //const title = "this is a setup title" // burasi reactivite degil
+
+      const title = ref("this is a setup title"); // simdi reactive oldu
+      const show = ref(false);
+      console.log(show);
+      const counter = ref("0");
+      const biggerThanZero = ref("Bigger than zero");
+      const smallerThanZero = ref("Smaller than zero");
+
+      const toggleIt = () =>{
+        show.value = !show.value;
+      } 
+      
+      return{
+        toggleIt,
+        biggerThanZero,
+        smallerThanZero,
+        counter,
+        title,
+        show,
+      }
+    }
+  }
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//vite plugin'ler burada tanimlaniyor.
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()]
