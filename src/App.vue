b8bb5f0ac198321d682d654e556d0adb939b093a
Tugran Kenger <turankenger@hotmail.com.tr>
2022-06-09 00:06:56 +0300

    use computed in setup

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
   <div>
     <!-- optiopns api ile composition api setup kullanilarak birlikte kullanilabilir. -->
 
-    {{ title }}
+    <h3>{{ title }}</h3>
 
     <input type="text" v-model="title"> <br><br>
+    {{  titleLengthMessage }}
+
     <button @click="toggleIt">Show/hide</button><br><br>
     <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore consectetur id odio harum voluptatum dolores optio ratione officiis obcaecati aut nam officia nesciunt cumque assumenda eius similique perspiciatis, eligendi quasi.</p>
 
-    <button @click="counter ++">+</button> &nbsp;
+    <button @click="counter --">-</button> &nbsp;
     <span>{{ counter }}</span> &nbsp;
-    <button @click="counter --">-</button> <br><br>
+    <button @click="counter ++">+</button> <br> <br> &nbsp;
     <span style="color:red" v-if="counter < 0">{{smallerThanZero}}</span>
     <span style="color:green" v-if="counter > 0">{{biggerThanZero}}</span>
   </div>
@@ -18,7 +20,7 @@
 
 <script>
 // reactive bir event icin {ref} import edilmeli(reactivite effect (ref) )
-import {ref} from "vue";
+import {ref, computed} from "vue";
   export default{
     // data(){
     //   return{
@@ -52,7 +54,13 @@ import {ref} from "vue";
       const toggleIt = () =>{
         show.value = !show.value;
       } 
-      
+
+      //computed fonksiyon gibi calisan degiskenlerdir. kullanimi :
+
+      const titleLengthMessage = computed(()=>{
+        return title.value.length + " characters was writed";
+      })
+
       return{
         toggleIt,
         biggerThanZero,
@@ -60,6 +68,7 @@ import {ref} from "vue";
         counter,
         title,
         show,
+        titleLengthMessage,
       }
     }
   }
